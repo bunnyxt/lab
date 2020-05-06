7fc4063ef1b29423249ef8ed79cd47265a4a6c96
bunnyxt <bunnyxt@outlook.com>
2020-05-06 23:37:41 +0800

    vue v-model vuex

---
+++ vue_v-model_vuex/InputComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- v-model bind to local computed property 'message' -->
+    <input v-model="message" placeholder="type something...">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input-component',
+  computed: {
+    message: {
+      // custome get() and set() of this local computed property
+      get () {
+        // get stored message
+        return this.$store.state.message;
+      },
+      set (newMessage) {
+        // commit a mutation to set message
+        this.$store.commit('setMessage', newMessage);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue_v-model_vuex/README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+A code template which using v-model to bind on a vuex stored variable by adding a local computed property as an agency.
+
+See more at blog [Vue v-model绑定到vuex state数据](https://www.bunnyxt.com/blog/framework/vue/508/).
